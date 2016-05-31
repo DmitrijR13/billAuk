9d77454affb9b2a3a65466154f1fa20c0c723b61
DmitrijR <DmitrijR@me.com>
2016-05-31 12:51:34 +0400

    Миграции: 1.Колонки;2.параметр по округлению пени

---
+++ Main/WIN/KP50.Migrator/KP50.DataBase.UpdateAssemly/2015/2015064/Migrarion_2016041906401.cs
@@ -0,0 +1,42 @@
+﻿﻿using KP50.DataBase.Migrator.Framework;
+using System.Collections.Generic;
+using System.Data;
+using MigrateDataBase = KP50.DataBase.Migrator.Framework.DataBase;
+
+namespace KP50.DataBase.UpdateAssembly._2015._2015064
+{
+    [Migration(2016041906401, MigrateDataBase.LocalBank)]
+    public class Migration_2016041906401 : Migration
+    {
+        public override void Apply()
+        {
+            SetSchema(Bank.Data);
+            Database.CommandTimeout = 6000;
+            var peni_debt = new SchemaQualifiedObjectName() { Name = "peni_debt", Schema = CurrentSchema };
+            var peni_calc = new SchemaQualifiedObjectName() { Name = "peni_calc", Schema = CurrentSchema };
+            if (Database.TableExists(peni_debt))
+            {
+                var columns = new List<string> { "sum_debt", "over_payments", "sum_debt_result", "sum_peni" };
+                foreach (var column in columns)
+                {
+                    Database.ExecuteNonQuery(string.Format("ALTER TABLE {0} ALTER COLUMN {1} TYPE NUMERIC ", peni_debt, column));
+                }
+                if (!Database.ColumnExists(peni_debt, "peni_rate"))
+                {
+                    Database.AddColumn(peni_debt, new Column("peni_rate", DbType.Decimal.WithSize(8, 5), ColumnProperty.None, 0));
+                }
+            }
+
+            if (Database.TableExists(peni_calc))
+            {
+                var columns = new List<string> { "sum_peni", "sum_old_reval", "sum_new_reval" };
+                foreach (var column in columns)
+                {
+                    Database.ExecuteNonQuery(string.Format("ALTER TABLE {0} ALTER COLUMN {1} TYPE NUMERIC ", peni_calc, column));
+                }
+            }
+        }
+    }
+}
+
+
---
+++ Main/WIN/KP50.Migrator/KP50.DataBase.UpdateAssemly/2015/2015064/Migrarion_2016042606401.cs
@@ -0,0 +1,49 @@
+﻿﻿using System;
+using System.Data;
+using KP50.DataBase.Migrator.Framework;
+using MigrateDataBase = KP50.DataBase.Migrator.Framework.DataBase;
+
+namespace KP50.DataBase.UpdateAssembly._2015._2015064
+{
+	// TODO: Set migration version as YYYYMMDDVVV
+	// YYYY - Year
+	// MM   - Month
+	// DD   - Day
+	// VVV  - Version
+	[Migration(2016042606401, MigrateDataBase.CentralBank | MigrateDataBase.LocalBank)]
+	public class Migration_2016042606401_CentralBank : Migration
+	{
+		public override void Apply()
+		{
+			SetSchema(Bank.Kernel);
+			var prm_name = new SchemaQualifiedObjectName() { Name = "prm_name", Schema = CurrentSchema };
+
+			if (Database.TableExists(prm_name))
+			{
+				if (NotExistRecord(prm_name, " WHERE nzp_prm=1700"))
+				{
+					Database.Insert(prm_name, new[] {"nzp_prm", "name_prm", "type_prm", "prm_num", "low_", "high_", "digits_"},
+						new[] {"1700", "Количество знаков для округления расчета пени", "int", "10", "0", "10", "1"});
+				}
+				else
+				{
+					Database.Update(prm_name, new[] {"nzp_prm", "name_prm", "type_prm", "prm_num", "low_", "high_", "digits_"},
+						new[] {"1700", "Количество знаков для округления расчета пени", "int", "10", "0", "10", "1" },
+						"nzp_prm=1700");
+				}
+			}
+		}
+
+		private bool NotExistRecord(SchemaQualifiedObjectName table, string where)
+		{
+			return Convert.ToInt32(
+				Database.ExecuteScalar("SELECT count(*) FROM " + GetFullTableName(table) + " " + where)) ==
+			       0;
+		}
+
+		private string GetFullTableName(SchemaQualifiedObjectName table)
+		{
+			return string.Format("{0}{1}{2}", table.Schema, Database.TableDelimiter, table.Name);
+		}
+	}
+}
